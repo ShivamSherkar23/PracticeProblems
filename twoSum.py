53cd47932ab4e647bce31ad8da70064011f39481
Shivam Sherkar <shivam23sherkar@gmail.com>
2020-09-13 11:35:12 +0530

    added two sum problem

---
+++ twoSum.py
@@ -0,0 +1,16 @@
+def twoSum(nums, target):
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            out = nums[i] + nums[j]
+            if out == target:
+                return [i, j]
+
+
+def main():
+    nums = [2, 7, 11, 15]
+    target = 9
+    result = twoSum(nums, target)
+    print('values at following index has sum equal to target ->', result)
+
+if __name__ == "__main__":
+    main()
